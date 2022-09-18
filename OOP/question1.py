ea262837ede232facaaa51a257b3231801c056fc
Cemal Emre Albayrak <70805503+emre570@users.noreply.github.com>
2022-09-18 03:18:33 +0300

    Delete OOP directory

--- OOP/question1.py
+++
@@ -1,7 +0,0 @@
-class Vehicle:
-    def __init__(self, maxSpeed, mileage):
-        self.maxSpeed = maxSpeed
-        self.mileage = mileage
-
-modelX = Vehicle(240, 18)
-print(modelX.maxSpeed, modelX.mileage)
